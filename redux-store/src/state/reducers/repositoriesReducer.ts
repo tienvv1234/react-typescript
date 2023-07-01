fb3471a62e151510cf1bb599b0240fe3173fe89e
tienvv1234 <tienvv1234@gmail.com>
2023-07-01 16:33:39 +0700

    add inteface for action in repositoriesReducer

--- redux-store/src/state/reducers/repositoriesReducer.ts
+++ redux-store/src/state/reducers/repositoriesReducer.ts
@@ -4,7 +4,12 @@ interface RepositoriesState {
     data: string[]
 }
 
-const reducer = (state: RepositoriesState, action: any): RepositoriesState => {
+interface Action {
+    type: string
+    payload: any
+}
+
+const reducer = (state: RepositoriesState, action: Action): RepositoriesState => {
     switch (action.type) {
         case 'search_repositories':
             return {
