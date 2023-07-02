3e270889adc0da2288f460489ec946d2eddf833f
tienvv1234 <tienvv1234@gmail.com>
2023-07-02 16:49:07 +0700

    onSubmit event to submit form

--- redux-store/src/components/RepositoriesList.tsx
+++ redux-store/src/components/RepositoriesList.tsx
@@ -1,10 +1,15 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 
 const RepositoriesList: FC = () => {
+    const [term, setTerm] = useState('');
+
+    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+    }
     return (
         <div>
-            <form>
-                <input type="text" />
+            <form onSubmit={onSubmit}>
+                <input value={term} onChange={(e) => setTerm(e.target.value)} />
                 <button>Search</button>
             </form>
         </div>
